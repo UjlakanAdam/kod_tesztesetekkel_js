670033dfebf37f674832898c8dcf947b336f1474
Ujlakán Ádám Sándor (SZF_N_2025_2027) <ujlakan.adam.sandor@diak.szamalk-szalezi.hu>
2026-02-25 15:13:15 +0100

    teszt

--- feladatok.js
+++ feladatok.js
@@ -1,5 +1,17 @@
 export function egyedi_betuk(szoveg = "") {
   // ide kell írni a megoldást ciklussal és elágazással
- 
-  return [];
+  const lista=[];
+  for (let index = 0; index < szoveg.length; index++) {
+    let betu = szoveg[index].toLowerCase();
+    if (/^a-záéíóöőúüű$/.test(betu) && lista.indexOf(betu)<0){
+      
+
+    lista.push(betu);
+    }
+
+  }
+
+  lista.sort()
+  console.log(lista)
+  return lista;
 }
